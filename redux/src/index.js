593ed8a7eec2df97ac971e075f1029510d0cee7f
sungyuha <yuha822@naver.com>
2022-11-15 21:52:38 +0900

    redux actions

--- redux/src/index.js
+++ redux/src/index.js
@@ -8,18 +8,27 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 
-
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
   //... modify state
   // state를 argument(인자)로 가지게 됨
-  return count;
+  console.log(action);
+  if (action.type === "ADD") {
+    //console.log("they are telling me to add one");
+    return count + 1;
+  } else if(action.type === "MINUS") {
+    return count -1;
+  } else {
+    return 0;
+  }
 };
 // data를 modify(찾고 수정하기)
 // countModifier가 1을 리턴하면, 
+// countModifier가 return하는 모든 것은, data가 됨
 
 const countStore = legacy_createStore(countModifier);
 // CreateStore는 reducer를 요구함
-console.log(countStore.getState());
+//console.log(countStore.getState());
+// data의 store를 만들고 (create하고), message를 그 store에 보내면(전송하면) 됨
 
 /*let count = 0;
 number.innerText = count;
@@ -46,3 +55,13 @@ const handleMinus = () => {
 
 //console.log(countStore.getStete());
 // reducer는 아주 중요
+
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "MINUS"});
+// store, dispatch, action을 말하면 리덕스가 countModifier를 부름
+
+console.log(countStore.getState());
