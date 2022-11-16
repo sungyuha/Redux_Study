d96727a7611380e0fb56380628ffbedb673e815f
sungyuha <yuha822@naver.com>
2022-11-16 18:30:03 +0900

    vanilla-redux

--- redux/src/index.js
+++ redux/src/index.js
@@ -11,7 +11,7 @@ const reducer = (state = [], action) => {
   console.log(action);
   switch(action.type) {
     case ADD_TODO:
-      return [...state, {text: action.text, id: action.id}]; // 새로운 array를 만듬 -> 과거의 state와 새로운 TODO를 갖고 있게 됨
+      return [...state, {text: action.text, id: action.id()}, ...state]; // 새로운 array를 만듬 -> 과거의 state와 새로운 TODO를 갖고 있게 됨
       // 이전 array의 컨텐츠로, 그리고 새로운 object로 array를 만듬
     case DELETE_TODO:
       return [];
@@ -36,12 +36,30 @@ const store = legacy_createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerText = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+  })
+}
+
+store.subscribe(paintToDos);
+
+const addToDo = (text) => {
+  store.dispatch({type: ADD_TODO, text})
+};
+
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value; // toDo 변수는 여기 이 input값에서 할당 됨
   input.value = "";
   //createToDo(toDo);
-  store.dispatch({type: ADD_TODO, text: toDo});
+  //store.dispatch({type: ADD_TODO, text: toDo});
+  addToDo(toDo);
 };
 // input에서 얻은 텍스트를 인자로 보냄
 // list item을 만들어 주고 list의 text를 받은 여기의 텍스트로 변경해줌
