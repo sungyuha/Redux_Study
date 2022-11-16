109a050e5dfa498a5a8e3107e07d71a9de69c357
sungyuha <yuha822@naver.com>
2022-11-16 13:38:28 +0900

    redux Recap Refactor

--- redux/src/index.js
+++ redux/src/index.js
@@ -9,22 +9,35 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
   //... modify state
   // state를 argument(인자)로 가지게 됨
   //console.log(action);
-  if (action.type === "ADD") {
+  /*if (action.type === "ADD") {
     //console.log("they are telling me to add one");
     return count + 1;
   } else if(action.type === "MINUS") {
     return count -1;
   } else {
     return count;
+  }*/
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case  MINUS:
+      return count - 1;
+    default:
+      return count;  
   }
 };
 // data를 modify(찾고 수정하기)
 // countModifier가 1을 리턴하면, 
 // countModifier가 return하는 모든 것은, data가 됨
+// action은 반드시 object여야함 -> action은 type이 있어야 함
+// action은 modifier와 communicate하는 방법
 
 const countStore = legacy_createStore(countModifier);
 // CreateStore는 reducer를 요구함
@@ -67,6 +80,14 @@ countStore.dispatch({type: "MINUS"});*/
 
 //console.log(countStore.getState());
 
+/*< Recap >
+✅ reducer : 현재 상태의 application과 함께 불려지는 function (+ with action)
+return하는 것은 application의 state가 됨
+✅ action : reducer와 소통하는 방법으로 Object여야 하며 그 key 이름은 항상 type임 (바꿀 수 없음)
+✅ dispatch : reducer에게 action을 보내는 방법
+✅ subscribe : store의 변화를 감지하면 인자값으로 준 함수를 실행
+✅ switch가 자주 쓰임*/
+
 const onChange = () => {
   number.innerText = countStore.getState();
 }
@@ -74,11 +95,11 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({type: "ADD"});
+  countStore.dispatch({type: ADD});
 };
 
 const handleMinus = () => {
-  countStore.dispatch({type: "MINUS"})
+  countStore.dispatch({type: MINUS});
 };
 
 add.addEventListener("click", handleAdd);
