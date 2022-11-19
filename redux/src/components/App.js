4f5b602d447c7834b0b61b64ce8706def79ca4fa
YUHASUNG <yuha822@naver.com>
2022-11-19 10:06:46 +0900

    react redux routes

--- redux/src/components/App.js
+++ redux/src/components/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import {Routes,Route} from "react-router-dom";
+import {BrowserRouter, Routes,Route} from "react-router-dom";
 import Detail from "../routes/Detail";
 import Home from "../routes/Home";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" exact component={Home}></Route>
-            <Route path="/id" component={Detail}></Route>
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact element={<Home />}></Route>
+                <Route path="/:id" element={<Detail />}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
