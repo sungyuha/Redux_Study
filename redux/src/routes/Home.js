87709d4db21b09db82afd41a2355a7126be520c8
sungyuha <yuha822@naver.com>
2022-11-21 14:21:00 +0900

    react redux To Do setup

--- redux/src/routes/Home.js
+++ redux/src/routes/Home.js
@@ -1,3 +1,26 @@
-const Home = () => "Home";
+import React, { useState } from "react"
+
+// const Home = () => "Home";
+
+function Home() {
+    const [text, setText] = useState("");
+    function onChange(e) {
+        setText(e.target.value);
+    }
+    function onSubmit(e) {
+        e.preventDefault();
+        setText("");
+    } 
+    return (
+        <>
+            <h1>To Do</h1>
+            <form onSubmit={onSubmit}>
+                <input type="text" value={text} onChange={onChange} />
+                <button>Add</button>
+            </form>
+            <ul></ul>
+        </>
+    )
+}
 
 export default Home;
