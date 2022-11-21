d96719d2b713e5e7ed184725a13b564e54f1d046
sungyuha <yuha822@naver.com>
2022-11-21 15:05:11 +0900

    react redux 1.Connecting the Store

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,6 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+//import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./components/App";
+import store from "./store";
+import {createRoot} from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+/*const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);*/
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+);
+
+/*ReactDOM.render(
+    <React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
+    // React Redux에는 Provider컴포넌트를 통해 앱의 다른 컴포넌트에서 Redux store를 사용할 수 있음
+    </React.StrictMode>,
+    document.getElementById("root")
+);*/
---
+++ redux/src/store.js
@@ -0,0 +1,40 @@
+import {legacy_createStore} from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+export const addToDo = text => {
+    return {
+        type: ADD,
+        text
+    };
+};
+
+export const deleteToDo = id => {
+    return {
+        type: DELETE,
+        id
+    };
+};
+
+const reducer = (state = [], action) => {
+    switch (action.type) {
+        case ADD:
+            return [{ text: action.text, id: Date.now() }, ...state];
+        case  DELETE:
+            return state.filter(toDo => toDo !== action.id);
+        default:
+            return state;    
+    }
+}
+
+const store = legacy_createStore(reducer);
+
+// action creator을 만들고
+// store.subscribe() function을 사용
+// 우리의 Application을 다시 render하고 싶음
+// react은 모든 부분을 render해주지 않음 -> 대신 변화가 있는 부분을 render해주면 됨
+// 그래서 subscribe를 하기 위해서 "redux-react" 가 필요한 시점
+// 왜냐하면 store의 변동사항에 대해 subscribe 하고 싶음
+// 그래서 모든 부분이 다시 render 되기를 원함
+export default store;
